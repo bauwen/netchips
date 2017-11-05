7930615210b39ad2b23613c508488c97f8cac048
bauwen <bauwen.demol@hotmail.com>
2017-11-05 23:15:41 +0100

    url series changed

--- netchips.js
+++ netchips.js
@@ -70,7 +70,7 @@ var procedures = [
 ];
 
 var URL_FILMS = "www.zmovie.tw";
-var URL_SERIES = "ewatchseries.to";
+var URL_SERIES = "xwatchseries.to";
 
 var DEFAULT_PORT = 3000;
 var callback = null;
@@ -343,6 +343,10 @@ function start(version) {
                                 data.episodes["http://" + URL_SERIES + episode.slice("http://mywatchseries.to".length)] = true;
                                 delete data.episodes[episode];
                             }
+                            else if (episode.indexOf("http://ewatchseries.to") == 0) {
+                                data.episodes["http://" + URL_SERIES + episode.slice("http://ewatchseries.to".length)] = true;
+                                delete data.episodes[episode];
+                            }
                         }
                     }
                     
@@ -366,6 +370,11 @@ function start(version) {
                             else if (serie.indexOf("http://mywatchseries.to") == 0) {
                                 data.series[serie].link = "http://" + URL_SERIES + data.series[serie].link.slice("http://mywatchseries.to".length);
                                 data.series["http://" + URL_SERIES + serie.slice("http://mywatchseries.to".length)] = data.series[serie];
+                                delete data.series[serie];
+                            }
+                            else if (serie.indexOf("http://ewatchseries.to") == 0) {
+                                data.series[serie].link = "http://" + URL_SERIES + data.series[serie].link.slice("http://ewatchseries.to".length);
+                                data.series["http://" + URL_SERIES + serie.slice("http://ewatchseries.to".length)] = data.series[serie];
                                 delete data.series[serie];
                             }
                         }
